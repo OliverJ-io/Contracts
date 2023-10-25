f6812e60f6685621da57e1a96f4bbf4ed830d9c0
olliejohnson <oliver.joseph.johnson@gmail.com>
2023-10-25 12:08:48 -0400

    aaaaaa

--- src/main/java/io/oliverj/contracts/data/ContractData.java
+++ src/main/java/io/oliverj/contracts/data/ContractData.java
@@ -1,26 +1,70 @@
 package io.oliverj.contracts.data;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
+import io.oliverj.contracts.Contracts;
+import org.apache.commons.compress.utils.Lists;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 public class ContractData {
 
     public static HashMap<String, String> contracts = new HashMap<String, String>();
+    public static HashMap<String, List<String>> contract_users = new HashMap<String, List<String>>();
+
+    public static List<String> users = Lists.newArrayList();
     public static void saveContract(String contractor, String contracted) {
-        contracts.put(contractor, contracted);
+        if(contractExists(contractor, contracted)) return;
+        String uuid = UUID.randomUUID().toString();
+        contracts.put(uuid, contracted);
+        saveContractUser(contractor, uuid);
         export_data();
     }
 
-    public static String getContract(String contractor) {
+    public static void saveContractUser(String contractor, String contractid) {
+        try {
+            import_data();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (contract_users.containsKey(contractor)) {
+            contract_users.get(contractor).add(contractid);
+        } else {
+            List<String> list = Lists.newArrayList();
+            list.add(contractid);
+            contract_users.put(contractor, list);
+            users.add(contractor);
+        }
+        export_data();
+    }
+
+    public static String getContract(String contractid) {
         try {
             import_data();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return contracts.get(contractor);
+        return contracts.get(contractid);
+    }
+
+    public static List<String> getUserContracts(String UUID) {
+        return contract_users.get(UUID);
+    }
+
+    public static String getContractId(String UUID, String targetUUID) {
+        for(String contract : getUserContracts(UUID)) {
+            if(Objects.equals(getContract(contract), targetUUID)) {
+                return contract;
+            }
+        }
+        return null;
+    }
+
+    public static Boolean contractExists(String contractor, String contracted) {
+        return getContract(getContractId(contractor, contracted)).equals(contracted);
     }
 
     private static void export_data() {
@@ -29,19 +73,32 @@ public class ContractData {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        JsonObject contracts = new JsonObject();
-        contracts.add("contracts", contracts);
+        Gson gson = new GsonBuilder().create();
+        StringBuilder contractString = new StringBuilder(gson.toJson(contracts));
 
-        try (FileWriter file = new FileWriter(ClassLoader.class.getResource("assets/contracts/store-data/contracts.json").toString())) {
-            file.write(contracts.toString());
+        for (String user : users) {
+            HashMap<String, List<String>> user_data = new HashMap<String, List<String>>();
+            user_data.put(user, getUserContracts(user));
+            String userString = gson.toJson(user_data);
+            contractString.append(userString);
+        }
+
+        try (FileWriter file = new FileWriter(ClassLoader.class.getResource("data/store-data/contracts.json").toString())) {
+            Contracts.LOGGER.info(contractString.toString());
+            file.write(contractString.toString());
             file.flush();
         } catch (IOException e) {
-            e.printStackTrace();
+            Contracts.LOGGER.error("Failed to write contract data: "+ e.toString());
         }
     }
 
     private static void import_data() throws FileNotFoundException {
         Gson gson = new Gson();
-        contracts = gson.fromJson(new BufferedReader(new FileReader(ClassLoader.class.getResource("assets/contracts/store-data/contracts.json").toString())).toString(), HashMap.class);
+        if (ClassLoader.class.getResource("data/store-data/contract.json") != null) {
+            HashMap jsondata = gson.fromJson(new BufferedReader(new FileReader(Objects.requireNonNull(ClassLoader.class.getResource("data/store-data/contracts.json")).toString())).toString(), HashMap.class);
+            contracts = (HashMap<String, String>) jsondata.get("contracts");
+            contract_users = (HashMap<String, List<String>>) jsondata.get("contract-users");
+            users = (List<String>) jsondata.get("users");
+        }
     }
 }
